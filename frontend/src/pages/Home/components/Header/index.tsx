7867e4a4b268c776322eeddf98025f38d1fb8e4d
haixiangyan <haixiang6123@gmail.com>
2021-10-26 00:52:17 +0800

    feat: 添加 banner image

--- frontend/src/pages/Home/components/Header/index.tsx
+++ frontend/src/pages/Home/components/Header/index.tsx
@@ -2,7 +2,7 @@ import styles from "./styles.less";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
+    <header className={styles.header} style={{ backgroundImage: "url('https://avatars.githubusercontent.com/u/17061654?v=4')" }}>
       <div className={styles.user}>
         <span className={styles.name}>海怪</span>
         <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/17061654?v=4" alt="头像"/>
